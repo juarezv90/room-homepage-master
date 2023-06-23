a9317240061c2c7a6475ac5987f4153370cb7fe9
Victor Juarez <juarezv1990@gmail.com>
2023-06-22 21:38:28 -0400

    add wheel event and script fixes

--- dist/script.js
+++ dist/script.js
@@ -2,22 +2,22 @@ const items = {
   titles: [
     "Discover innovative ways to decorate",
     "We are available all across the globe",
-    "Manufactured with the best materials",
+    "Manufactured with the best materials"
   ],
   articles: [
     "We provide unmatched quality, comfort, and style for property owners across the country. Our experts combine form and function in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
     "With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
-    " Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
+    " Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
   ],
   mobileImages: [
     "./images/mobile-image-hero-1.jpg",
     "./images/mobile-image-hero-2.jpg",
-    "./images/mobile-image-hero-3.jpg",
+    "./images/mobile-image-hero-3.jpg"
   ],
   desktopImages: [
     "./images/desktop-image-hero-1.jpg",
     "./images/desktop-image-hero-2.jpg",
-    "./images/desktop-image-hero-3.jpg",
+    "./images/desktop-image-hero-3.jpg"
   ]
 };
 
@@ -25,8 +25,7 @@ let width = window.innerWidth;
 
 window.addEventListener("resize", () => {
   width = this.innerWidth;
-})
-
+});
 
 const leftTrigger = document.getElementById("leftArrow");
 const rightTrigger = document.getElementById("rightArrow");
@@ -42,16 +41,20 @@ const nextItem = () => {
   index = index < items.titles.length - 1 ? index + 1 : 0;
   document.getElementById("title").innerHTML = items.titles[index];
   document.getElementById("article").innerHTML = items.articles[index];
-  header.style.backgroundImage = `url(${width < 500 ? items.mobileImages[index] : items.desktopImages[index]})`;
+  header.style.backgroundImage = `url(${
+    width < 500 ? items.mobileImages[index] : items.desktopImages[index]
+  })`;
 };
 const prevItem = () => {
   index = index > 0 ? index - 1 : items.titles.length - 1;
   document.getElementById("title").innerHTML = items.titles[index];
   document.getElementById("article").innerHTML = items.articles[index];
-  header.style.backgroundImage = `url(${width < 500 ? items.mobileImages[index] : items.desktopImages[index]})`;
+  header.style.backgroundImage = `url(${
+    width < 500 ? items.mobileImages[index] : items.desktopImages[index]
+  })`;
 };
 
-rightTrigger.onclick = () => {
+const handleNext = () => {
   clearInterval(interval);
   nextItem();
   interval = setInterval(() => {
@@ -59,10 +62,22 @@ rightTrigger.onclick = () => {
   }, 5000);
 };
 
-leftTrigger.onclick = () => {
+const handlePrev = () => {
   clearInterval(interval);
   prevItem();
   interval = setInterval(() => {
     nextItem();
   }, 5000);
 };
+
+rightTrigger.onclick = handleNext;
+
+leftTrigger.onclick = handlePrev
+
+document.getElementById('grid-box-two').addEventListener("wheel", (e) => {
+  if (e.deltaY < 0) {
+    handlePrev()
+  } else if (e.deltaY > 0) {
+    handleNext()
+  }
+});
